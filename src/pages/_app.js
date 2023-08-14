d302b6747de6bdf0c34b6b1158d4ed061ff65514
davidasix <davidandersonsix@gmail.com>
2023-08-14 18:45:23 -0400

    Properly formatted Head for html content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,23 @@
-import "src/styles/theme.css";
+import Head from 'next/head';
+import 'src/styles/theme.css';
+import 'src/styles/globals.css';
 import 'src/styles/bootstrap.css';
-import 'src/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>DavidASix</title>
+        <meta name="description" content="The portfolio and online presence of David A Six." />
+
+        <script async src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossOrigin="anonymous"></script>
+        <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossOrigin="anonymous"></script>
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Major+Mono+Display&display=swap" rel="stylesheet" />
+        <link href="https://fonts.cdnfonts.com/css/baron-neue" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
-      <Head>
-        <script async src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossOrigin="anonymous"></script>
-        <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossOrigin="anonymous"></script>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        {/*
-        <link href="https://fonts.googleapis.com/css2?family=Copse&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Laila:wght@300&display=swap" rel="stylesheet">
-        */}
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Major+Mono+Display&display=swap" rel="stylesheet" />
-        <link href="https://fonts.cdnfonts.com/css/baron-neue" rel="stylesheet" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
