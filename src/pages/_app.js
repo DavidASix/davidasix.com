7ed73ebc5d9069252a532239f438978245f99778
davidasix <davidandersonsix@gmail.com>
2023-08-15 22:47:59 -0400

    Reconfigured heads to fit Next12 best practices
    Removed bootstrap deps from npm, included cdn scripts in _app instead

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Script from 'next/script'
+
 import 'src/styles/bootstrap.css';
 import 'src/styles/theme.css';
 import 'src/styles/globals.css';
@@ -10,13 +12,8 @@ export default function App({ Component, pageProps }) {
         <title>DavidASix</title>
         <meta name="description" content="The portfolio and online presence of David A Six." />
 
-        <script async src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossOrigin="anonymous"></script>
-        <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossOrigin="anonymous"></script>
-        
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Major+Mono+Display&display=swap" rel="stylesheet" />
-        <link href="https://fonts.cdnfonts.com/css/baron-neue" rel="stylesheet" />
       </Head>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" />
       <Component {...pageProps} />
     </>
   );
