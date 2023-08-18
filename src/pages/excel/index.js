dc68b3b306ed9e65c0cdc9b0a1e34c932473a3d9
davidasix <davidandersonsix@gmail.com>
2023-08-17 21:18:43 -0400

    Reworked layout to accomidate more mobile optimization
    Updated copy

--- src/pages/excel/index.js
+++ src/pages/excel/index.js
@@ -54,14 +54,17 @@ export default function Excel({formulas_str}) {
 
         <section className={`row d-flex justify-content-center align-items-start ${cs.maxSection} ${cs.heroSection} ${cs.stickyParent} p-0`}>
           <div className={`col-12 p-0 ${cs.center} flex-column`} style={{zIndex: 30}}>
-            <h2 className={`m-3 headerFont display-1 text-center`}>Hall of Formulas</h2>
-            <p style={{textAlign: 'center'}}>
+            <h2 className={`m-3 mb-0 headerFont display-1 text-center`}>Microsoft Excel</h2>
+            <h3 className={`m-3 mt-0 headerFont display-6 text-center`}>and the Hall of Formulas</h3>
+            <p className={`text-center d-none d-lg-flex`}>
+              Excel is a tool I think everyone should know a little about, and one that I know a lot about.
+              <br />
               In my many years working with Excel I have created and uncovered some clever formulas.
             <br />
-              I’ve included some of my favourites works below to impress and amaze.
+              I’ve included some of my favorite works below to impress and amaze.
             </p>
           </div>
-          <div className='col-lg-8 row m-0 me-lg-2'>
+          <div className='col-xl-8 row m-0 order-xl-1 order-2'>
             {formulas.map((formula, i) => (
               <React.Fragment key={i}>
                 <ExcelFormula {...formula} />
@@ -69,14 +72,23 @@ export default function Excel({formulas_str}) {
             ))}
           </div>
           <div
-            className={`d-lg-flex  d-none col-lg-2 ms-3 row sticky-top`}
+            className={`d-flex col-xl-3 row sticky-xl-top order-xl-2 order-1`}
             style={{ zIndex: 30, top: 40 + 8 + 8 + 16 + 16 }}
           >
-            <div className={`${cs.frosted} col-12 rounded-3 d-flex flex-column p-3`}>
-              <h5 className="headerFont m-0">Some thoughts on Excel</h5>
-              <p>Here are some thoughts on Excel and my relationship with it</p>
+            <div className={`${cs.frosted} col-12 rounded-3 d-flex flex-column p-3 mb-3`}>
+              <h5 className="headerFont m-0 mb-1">Excel & Me</h5>
+              <p>Some people have a love hate relationship with Spreadsheets; I have a love love relationship with them.</p>
+              <p>Over my many years of experience with Excel I've always challenged myself to push the boundaries, making sheets and reports that are as small as possible while still being easy for a user to utilize</p>
+              <p>I've been an Excel beta feature tester for years, subscribing on my personal Microsoft account as trying out the new features is so fun.</p>
+              <h6 className="headerFont">Excel Highlights</h6>
+              <ul>
+                <li>Creating reports for C level executives in Canadian Telecom</li>
+                <li>Creating reproducible accountability reporting to effect actual change in a large company</li>
+                <li>Selling hundreds of spreadsheet reports online due to great UI design</li>
+                <li>Pushing Excel's capabilities with Power Query, Data Models, 3D maps, and advanced functions</li>
+              </ul>
               <h6 className="headerFont">TL;DR</h6>
-              <p>Shorter thoughts</p>
+              <p>Excel rocks and I rock at Excel.</p>
             </div>
           </div>
 
