92c3a22fb16ea02ba68175c64e0dd55b972ed91c
davidasix <davidandersonsix@gmail.com>
2023-12-30 11:11:34 -0500

    Updated home page styles, removed firebase reliance

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,88 +1,80 @@
 import Head from 'next/head';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 import s from "src/pages/home.module.css";
 import cs from "src/styles/common.module.css";
+
+import constants from 'src/assets/constants';
 
 import NavigationLayout from 'src/components/NavigationLayout/';
 import SocialIcon from 'src/components/SocialIcon/';
 import Circles from "public/images/shapes/circle-scatter.svg";
 
-// Initialize Firebase
-import firebaseConfig from 'src/assets/firebase-config.json';
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const socials = require('/src/assets/socials.json');
 
 const SocialLink = ({social}) => {
   return (
     <a 
       href={social.url} 
-      className={`rounded-4 mx-2 ${cs.center} ${cs.frosted} ${cs.grow} ${s.socialContainer}`}>
-      <SocialIcon social={social.socialMedia} style={{height: "70%", width: "70%", fill: "var(--fg)" }} />
+      className={`rounded-4 mx-2 hover hover-primary  row frosted justify-content-center align-items-center`}>
+      <SocialIcon social={social.socialMedia} className='hover' style={{height: 70, width: 70 }} />
     </a>
   );
 }
 
-async function getSocialAccounts() {
-  const socialMediaCollection = collection(db, 'social-media');
-  let socialAccounts = await getDocs(socialMediaCollection)
-  socialAccounts = socialAccounts.docs.map(doc => doc.data())
-  return socialAccounts;
-}
-
-export const getServerSideProps = async () => {
-  let socials = await getSocialAccounts();
-  return { props: { socials } }
-}
- 
-export default function Home({socials}) {
+export default function Home(props) {
   return (
     <>
       <Head>
+        <title>{`${constants.siteName}`}</title>
       </Head>
       <NavigationLayout>
-        <section className={`${cs.center} ${cs.maxSection} ${cs.heroSection} row flex-md-row-reverse px-2`}>
-          <div className={`col-12 col-md-6 position-relative flex-column pt-md-0 pt-3 ${cs.center}`} style={{zIndex: 20}}>
-            <div className={`${s.headshotContainer}`}>
-              <img
-                src="/images/headshot_bg.svg"
-                alt="An orange circle behind image of David"
-                className={`${s.headshot}`}
-              />
-              <img
-                src="/images/headshot.png"
-                alt="A headshot of David wearing an unbuttoned white collared shirt."
-                className={`${s.headshot}`}
-              />
-            </div>
-            <div className={`${s.socialSpacer}`} />
-            <div className={`d-flex ${cs.center} ${s.socialRow}`}>
-              {socials.map((s, i) => <SocialLink key={i} social={s} />)}
+        <section className={`col-12 row flex-md-row-reverse 
+        position-relative 
+        justify-content-center align-items-center align-content-center        
+        nav-padding ${s.hero}`}>
+          <img 
+            src='/images/shapes/gradient-bg.png' 
+            alt='A gradient background'
+            className={s.gradient} />
+
+          <div className={`col-12 col-md-6 row justify-content-center position-relative m-0 px-3`}>
+            <img
+              src="/images/headshot_bg.svg"
+              alt="An orange circle behind image of David"
+              style={{zIndex: 0, position: 'absolute', bottom: 0}}
+              className={`${s.headshot} px-3`}
+            />
+            <img
+              src="/images/headshot.png"
+              alt="A headshot of David wearing an unbuttoned white collared shirt."
+              style={{zIndex: 15}}
+              className={`${s.headshot} px-3`}
+            />
+            <div className={`${s.socialRow}`} style={{zIndex: 20}}>
+              {socials.map((s, i) => <SocialLink key={i} social={s} color='white' />)}
             </div>
           </div>
 
-          <div className={`col-12 col-md-6 position-relative ${cs.center} p-1 py-3`} style={{zIndex: 10}}>
-              <Circles fill="#FFFFFF25" stroke="#FFFFFF80" strokeWidth={1} style={{ width: "125%", minWidth: 750}} className={`position-absolute`} />
-            <div className={`d-flex flex-column justify-content-center rounded-4 p-3 ${cs.frosted}`}>
-              <span className={`display-6 m-0`}>Hi there, I’m</span>
-              <h1 className={`display-4 m-0`}>David A Six</h1>
-              <br />
-              <span>I’m a developer, maker, and tech enthusiast.</span>
-              <span>
+          <div className={`col-12 col-md-6 position-relative row justify-content-center align-items-center align-content-center ps-5`}>
+            <div className={`row rounded-5 frosted p-2`} style={{zIndex: 20, maxWidth: 700 }}>
+              <span className={`fs-3 fw-lighter headerFont`}>
+                Hi there, I’m
+              </span>
+              <h1 className={`fs-d5 headerFont`}>David A Six</h1>
+              <span className='mt-2'>
+                I’m a developer, maker, and tech enthusiast.
+              </span>
+              <span className='mt-2'>
                 Why the six? I’m the sixth David Anderson in my family tree!
               </span>
-              <br />
-              <span>
-                Curious about what I bring to the table? Check out my projects and skills above!
+              <span className='my-2'>
+                Need a developer or data expert with proven soft and hard skills? Look no further!
               </span>
             </div>
           </div>
+
         </section>
       </NavigationLayout>
     </>
   );
 }
-
-    
