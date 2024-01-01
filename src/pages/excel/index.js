173867d06d637fc73a0748f40c07a784a3da0de2
davidasix <davidandersonsix@gmail.com>
2024-01-01 11:32:05 -0500

    Converted formulas to lazy loading, added skeleton while loading

--- src/pages/excel/index.js
+++ src/pages/excel/index.js
@@ -1,49 +1,80 @@
 import Head from 'next/head';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-import React from 'react'
-import cs from "src/styles/common.module.css";
+import React, { useState, useEffect } from 'react'
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
+
 
 import NavigationLayout from 'src/components/NavigationLayout/';
+import { Formula, FormulaSkeleton } from 'src/components/Excel/';
+
+import { db } from "src/components/Firebase";
 
 // Initialize Firebase
-import firebaseConfig from 'src/assets/firebase-config.json';
 import constants from 'src/assets/constants';
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const formulasPerPage = 4;
 
-// Get a list of cities from your database
-async function getExcelFormulas() {
-  const formulaCollection = collection(db, 'formulas');
-  let formulas = await getDocs(formulaCollection)
-  formulas = formulas.docs.map(doc => doc.data())
-  return formulas;
-}
-
-const ExcelFormula = ({title, description, formula_with_whitespace}) => (
-  <div className={`container-fluid col-12 px-3`}>
-    <h2 className={``}>{title}</h2>
-    <p className={``}>
-      {description}
-    </p>
-    <div className={`p-3 mt-3 mb-4 d-flex align-items-center frosted rounded-4`}>
-      <code className={`p-0 m-0`}>
-        <pre className="m-0" style={{whiteSpace: 'pre-wrap', wordBreak: 'break-all'}}>{formula_with_whitespace}</pre>
-      </code>
-    </div>
-  </div>
-);
-
-export const getServerSideProps = async ({ searchParams }) => {
+const getFormulas = (page) => new Promise(async (resolve, reject) => {
+  const currentPage = (page || 1) * 1;
   // Get Blog snapshot
-  let formulas_str = await getExcelFormulas();
-  formulas_str = JSON.stringify(formulas_str);
-  return { props: { formulas_str } }
-}
+  const formulaCollection = collection(db, "formulas");
+  const q = query(formulaCollection, 
+    //where("status", "==", "published"),
+    orderBy("created_date", "desc"));
+  const snapshot = await getDocs(q);
 
-export default function Excel({formulas_str}) {
-  const formulas = JSON.parse(formulas_str);
+  // Get current page of formulas
+  let formulas = snapshot.docs.slice(
+    (currentPage - 1) * formulasPerPage,
+    currentPage * formulasPerPage
+  );
+
+  formulas = formulas.map((doc, i) => doc.data());
+  formulas = await Promise.all(formulas);
+  return resolve(formulas);
+});
+
+const getMaxPage = () => new Promise(async (resolve, reject) => {
+  const formulaCollection = collection(db, "formulas");
+  const q = query(formulaCollection);
+  const snapshot = await getDocs(q);
+  // Organize pagination information
+  const numberOfPages = Math.ceil(snapshot.size / formulasPerPage);
+  return resolve(numberOfPages);
+});
+
+export default function Excel() {
+  const [formulas, setFormulas] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(null);
+  const [loadingNextPage, setLoadingNextPage] = useState();
+
+  useEffect(() => {
+    // Likely does not need to be Async, but I might add an async call here later
+    const setInitialFormulas = async () => {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      const fetchedFormulas = await getFormulas(1);
+      setFormulas(fetchedFormulas)
+      const maxPageFetch = await getMaxPage();
+      setMaxPage(maxPageFetch);
+    }
+    setInitialFormulas();
+  }, []);
+
+  async function loadNextPage() {
+    try {
+      setLoadingNextPage(true);
+      const nextPage = currentPage + 1;
+      const nextPageFormulas = await getFormulas(nextPage);
+      setFormulas([...formulas, ...nextPageFormulas])
+      setCurrentPage(nextPage);
+    } catch (err) {
+      alert('Could not find more formulas.')
+      console.log(err)
+    } finally {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      setLoadingNextPage(false);
+    }
+  }
   return (
     <>
       <Head>
@@ -51,7 +82,7 @@ export default function Excel({formulas_str}) {
       </Head>
       <NavigationLayout>
 
-        <section className={`nav-padding col-12 row justify-content-center align-items-start ${cs.stickyParent}`}>
+        <section className={`nav-padding col-12 row justify-content-center align-items-start stickyParent`}>
           <div className={`col-10 col-lg-8 row justify-content-center`} style={{zIndex: 30}}>
             <h1 className={`fs-d1 text-center text-nowrap`}>
               Microsoft Excel
@@ -62,15 +93,15 @@ export default function Excel({formulas_str}) {
               In my many years working with Excel I have created and uncovered some clever formulas. I’ve included some of my favorite works below to impress and amaze.
             </p>
           </div>
-            <h3 className={`fs-1 col-12 col-lg-10`}>
+            <h3 className={`fs-1 col-12 col-lg-10 mb-2`}>
               The Hall of Formulas
             </h3>
           <div className='col-12 col-lg-6 row order-lg-1 order-2'>
-            {formulas.map((formula, i) => (
-              <React.Fragment key={i}>
-                <ExcelFormula {...formula} />
-              </React.Fragment>
-            ))}
+            {/* If there are no formulas loaded yet, render a skeleton layout. */}
+            {maxPage === null && Array.from({ length: 3 })
+              .map((_, i) => <FormulaSkeleton index={i} key={i} />)}
+            {formulas.map((formula, i) => <Formula {...formula} key={i} /> )}
+            {loadingNextPage && <FormulaSkeleton />}
           </div>
           <div
             className={`d-flex col-lg-4 px-3 row sticky-lg-top order-lg-2 order-1`}
