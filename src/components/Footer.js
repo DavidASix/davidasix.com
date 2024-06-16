01f8919e346d74b02aac46569f4b88fa2b7e72fe
David Anderson <davidandersonsix@gmail.com>
2024-06-16 17:23:05 -0400

    Converted Footer to Tailwind styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,36 @@
-import c from "/src/assets/constants";
-import s from "./Footer.module.css";
+import Link from "next/link";
+import c from "@/assets/constants";
 
 export default function Footer() {
   return (
-      <div className={`col-12 row ${s.footer} justify-content-center align-items-center`}>
-        <div className="col-11 col-md-10 d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
-          <div className="col-md-8 d-flex align-items-center">
-            <a
-              href="/"
-              className={`d-md-flex d-none me-4`}
-              style={{ height: 25, width: 25}}
-            >
-              <img
-                src="/favicon.ico"
-                className='img-fluid'
-                alt="Home Icon"
-              />
-            </a>
-            <div className="d-flex flex-column justify-content-center align-items-start">
-              <span className="fs-6">
-                © {new Date().getFullYear()} {c.siteName}, all rights reserved
-              </span>
-            </div>
-          </div>
-          
-          <div className={`d-flex col-12 col-md-4 justify-content-end`}>
-            <a 
-              href="http://redoxfordonline.com" 
-              className="fst-italic fs-small text-end hover hover-muted">
-              Site created by RedOxfordOnline
-            </a>
+    <div className={`w-full min-h-48 ${c.sectionPadding} justify-center`}>
+      <div
+        className={`${c.contentContainer} w-full py-8 
+        flex flex-col md:flex-row justify-between items-start`}
+      >
+        <div className="flex justify-center items-center">
+          <Link href="/" className={`h-10 w-10 me-2`}>
+            <img src="/favicon.ico" alt="Home Icon" />
+          </Link>
+          <div className="flex flex-col justify-center items-start space-y-1">
+            <span className="text-sm text-da-dark-50">David Anderson Six</span>
+            <span className="text-sm text-da-dark-50">Waterloo, Ontario</span>
           </div>
         </div>
+        <div
+          className={`flex flex-col justify-end items-end self-end md:self-auto`}
+        >
+          <a
+            href="https://redoxfordonline.com/projects/?utm_source=davidasix&utm_medium=footer_menu&utm_campaign=referral"
+            className="text-sm text-da-dark-50 hover:text-red-900 transition-all duration-150"
+          >
+            Red Oxford Online
+          </a>
+          <span className="text-sm text-ku-dark-muted">
+            ©{new Date().getFullYear()} {c.siteName}, all rights reserved
+          </span>
+        </div>
       </div>
+    </div>
   );
 }
